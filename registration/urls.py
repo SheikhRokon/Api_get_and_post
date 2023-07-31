a6ad5a58c4d6210ab96adbc8053ef052cefb9d9b
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-07-31 15:06:47 +0600

    api time limit

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from ApiProject.views import MyTokenObtainPairView
 
 urlpatterns = [
    path('registetionApi/',registetionApi),
-   path('registetionApi/token/',TokenObtainPairView.as_view()),
+   path('registetionApi/token/',MyTokenObtainPairView.as_view()),
    path('registetionApi/token/referstoken/',TokenRefreshView.as_view()),
 
 ]
